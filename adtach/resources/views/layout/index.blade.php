fc4d148d3c8b81ffd7cf0ade78184af8bfc1ed99
Muhammad-work <work47687@gmail.com>
2024-12-24 22:25:37 +0500

    add new futuer

--- adtach/resources/views/layout/index.blade.php
+++ adtach/resources/views/layout/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> PBS Digitel </title>
+    <title> PBS Digitals </title>
     <link rel="shortcut icon" href="{{ asset('storage/img/logo-2.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
